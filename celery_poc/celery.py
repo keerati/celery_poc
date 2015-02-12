42c2f1b294e2fee90bf876b914a6e69b375d4edb
keerati <keerati@acommerce.asia>
2015-02-12 14:48:06 +0700

    Play with postrun signal

--- celery_poc/celery.py
+++ celery_poc/celery.py
@@ -5,9 +5,11 @@ import json
 
 from celery import Celery
 from celery.utils.log import get_task_logger
+from celery.signals import task_postrun
 
 from django.conf import settings
 from time import sleep
+import requests
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celery_poc.settings')
 
@@ -29,3 +31,16 @@ def print_json_for_feed(self, data):
     logger.info('start print_json_for_feed')
     sleep(10)
     logger.info('got data %s' % data)
+
+
+# ========== signals ===============
+@task_postrun.connect(sender = print_json)
+def post_to_websocket_server(**kwargs):
+    logger.debug('post to websocket server!')
+    headers   = {'content-type' : 'application/json' }
+    post_dict = { "msg" : "req"}
+    post_data = json.dumps(post_dict)
+    response  = requests.post(
+            "http://localhost:30000/post",
+            data=post_data, headers=headers)
+    logger.debug(response.text)
